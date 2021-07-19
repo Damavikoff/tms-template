f3c1425847fbd9386813db769742f7449a1ed6d5
Damavikoff <damavikoff@gmail.com>
2021-07-19 15:17:00 +0300

    whole Sunday

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 const gulp = require('gulp');
 const styles = require('./gulp/styles');
+const js = require('./gulp/js');
 const fonts = require('./gulp/fonts');
 const image = require('./gulp/image');
 const html = require('./gulp/html');
 const watchDev = require('./gulp/watchDev');
 
-const build = gulp.parallel(html, styles, fonts, image);
+const build = gulp.parallel(html, styles, js, fonts, image);
 
 module.exports.start = gulp.series(build, watchDev);
 module.exports.build = gulp.series(build);
